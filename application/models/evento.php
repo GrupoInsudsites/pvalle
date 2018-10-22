4a8020ef60f3549700aadb9165bf4a33d41cf996
fernandopriet1997 <diegoprieto199796@gmail.com>
2018-10-22 14:24:37 -0300

    Validador de respuestas

--- application/models/evento.php
+++ application/models/evento.php
@@ -299,7 +299,11 @@ public function fetch_invitados_hoy_llegaron( $criterio='') {
         if(isset($validation[0])){
             if($validation[0]->salida == null && $data->salida != null){
                 $this->db->set('salida', $data->salida);
-                $this->db->set('patente_salida', $data->patente_salida);
+                if($data->patente_salida != null){
+                    $this->db->set('patente_salida', $data->patente_salida);
+                }else{
+                    $this->db->set('patente_salida', "");
+                }
                 $this->db->where('id', $validation[0]->id);
                 $this->db->update('entradasalida');
             }
